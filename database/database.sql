54c0be0276b2c72599fca9911a3f7fda07a30cfd
nilfern <nilfern@gmail.com>
2024-01-14 09:49:31 -0500

    actualizacion archivo databaes

--- database/database.sql
+++ database/database.sql
@@ -1,116 +1,119 @@
-CREATE TABLE language (
-                          id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                          name varchar(255) UNIQUE NOT NULL,
-                          ISO_code varchar(255) UNIQUE NOT NULL
-);
+ CREATE TABLE IF NOT EXISTS language (
+                id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                name varchar(255) UNIQUE NOT NULL,
+                ISO_code varchar(255) UNIQUE NOT NULL,
+                UNIQUE (id)
+            );
 
-CREATE TABLE serie (
-                       id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                       title varchar(255) NOT NULL,
-                       platform_id integer NOT NULL,
-                       review text NOT NULL
-);
+CREATE TABLE IF NOT EXISTS serie (
+                id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                title varchar(255) NOT NULL,
+                serie_img varchar(255) NOT NULL,
+                platform_id INT NOT NULL,
+                review text NOT NULL,
+                UNIQUE (id)
+            );
 
-CREATE TABLE platform (
-                          id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                          name varchar(255) NOT NULL,
-                          image varchar(255) NOT NULL
-);
+  CREATE TABLE IF NOT EXISTS platform (
+                id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                name varchar(255) NOT NULL,
+                image varchar(255) NOT NULL,
+                UNIQUE (id)
 
-CREATE TABLE director (
-                          id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                          name varchar(255) NOT NULL,
-                          surname varchar(255) NOT NULL,
-                          birthdate timestamp NOT NULL,
-                          nationality varchar(255) NOT NULL
-);
+            );
 
-CREATE TABLE actor (
-                       id integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-                       name varchar(255) NOT NULL,
-                       surname varchar(255) NOT NULL,
-                       birthdate timestamp NOT NULL,
-                       nationality varchar(255) NOT NULL
-);
+ CREATE TABLE IF NOT EXISTS director (
+                id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                name varchar(255) NOT NULL,
+                surname varchar(255) NOT NULL,
+                birthdate DATE NOT NULL,
+                nationality varchar(255) NOT NULL,
+                UNIQUE (id)
+            );
 
-CREATE TABLE actuacion (
-                           serie_id integer NOT NULL ,
-                           actor_id integer NOT NULL ,
-                           PRIMARY KEY (serie_id, actor_id)
-);
+  CREATE TABLE IF NOT EXISTS actor (
+                id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+                name varchar(255) NOT NULL,
+                surname varchar(255) NOT NULL,
+                birthdate DATE NOT NULL,
+                nationality varchar(255) NOT NULL,
+                UNIQUE (id)
+            );
 
-CREATE TABLE direccion (
-                           director_id integer NOT NULL ,
-                           serie_id integer NOT NULL ,
-                           PRIMARY KEY (director_id, serie_id)
-);
+ CREATE TABLE IF NOT EXISTS actuacion (
+                serie_id INT NOT NULL ,
+                actor_id INT NOT NULL ,
+                PRIMARY KEY (serie_id, actor_id)
+            );
 
-CREATE TABLE available_in (
-                              serie_id integer NOT NULL ,
-                              platform_id integer NOT NULL ,
-                              PRIMARY KEY (serie_id, platform_id)
-);
+ CREATE TABLE IF NOT EXISTS direccion (
+                director_id INT NOT NULL ,
+                serie_id INT NOT NULL ,
+                PRIMARY KEY (director_id, serie_id)
+            );
 
-CREATE TABLE audio_languages (
-                                 serie_id integer NOT NULL ,
-                                 language_id integer NOT NULL ,
-                                 PRIMARY KEY (serie_id, language_id)
-);
 
-CREATE TABLE subtitles_languages (
-                                     serie_id integer NOT NULL ,
-                                     language_id integer NOT NULL ,
-                                     PRIMARY KEY (serie_id, language_id)
-);
 
--- Ejemplo con CASCADE
-ALTER TABLE available_in
-    ADD FOREIGN KEY (platform_id) REFERENCES platform (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+ CREATE TABLE IF NOT EXISTS audio_languages (
+                serie_id INT NOT NULL ,
+                language_id INT NOT NULL ,
+                PRIMARY KEY (serie_id, language_id)
+            );
 
-ALTER TABLE available_in
-    ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+CREATE TABLE IF NOT EXISTS subtitles_languages (
+                serie_id INT NOT NULL ,
+                language_id INT NOT NULL ,
+                PRIMARY KEY (serie_id, language_id)
+            );
 
-ALTER TABLE actuacion
-    ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+-- Ejemplo con CASCADE--
+ ALTER TABLE serie
+                ADD FOREIGN KEY (platform_id) REFERENCES platform (id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE;
 
-ALTER TABLE actuacion
-    ADD FOREIGN KEY (actor_id) REFERENCES actor (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+ 
+            ALTER TABLE actuacion
+                ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
 
-ALTER TABLE direccion
-    ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+            ALTER TABLE actuacion
+                ADD FOREIGN KEY (actor_id) REFERENCES actor (id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
 
-ALTER TABLE direccion
-    ADD FOREIGN KEY (director_id) REFERENCES director (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+         
+            ALTER TABLE direccion
+                ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
 
-ALTER TABLE audio_languages
-    ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+            ALTER TABLE direccion
+                ADD FOREIGN KEY (director_id) REFERENCES director (id)
+                    ON UPDATE CASCADE
+                    ON DELETE RESTRICT
 
-ALTER TABLE audio_languages
-    ADD FOREIGN KEY (language_id) REFERENCES language (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+          
+            ALTER TABLE audio_languages
+                ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
 
-ALTER TABLE subtitles_languages
-    ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+        
+            ALTER TABLE audio_languages
+                ADD FOREIGN KEY (language_id) REFERENCES language (id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
 
-ALTER TABLE subtitles_languages
-    ADD FOREIGN KEY (language_id) REFERENCES language (id)
-        ON UPDATE CASCADE
-        ON DELETE CASCADE;
+       
+            ALTER TABLE subtitles_languages
+                ADD FOREIGN KEY (serie_id) REFERENCES serie (id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
+
+            ALTER TABLE subtitles_languages
+                ADD FOREIGN KEY (language_id) REFERENCES language (id)
+                    ON UPDATE CASCADE
+                    ON DELETE CASCADE
 
